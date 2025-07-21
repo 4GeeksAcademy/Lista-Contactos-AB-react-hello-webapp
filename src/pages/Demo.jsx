09f5e01d55002b8cd1ea8b9f5280b011900a01c8
andersontbernal <andersonbernal7@gmail.com>
2025-07-21 02:35:04 +0000

    Siguiente paso: volver a vista 1 y refactorizar el codigo para volver funcional la lista de contactos guardando datos y llevandolos a la vista 2.

--- src/pages/Demo.jsx
+++ src/pages/Demo.jsx
@@ -9,37 +9,35 @@ export const Demo = () => {
   return (
     <div className="container">
       <div className="d-flex justify-content-end mb-4">
-        <Link to="/"> 
+        <Link to="/">
           <button className="btn btn-success">Add new contact</button>
         </Link>
       </div>
 
-      <ul className="list-group">
-        {/* Map over the 'todos' array from the store and render each item as a list element */}
-        {store && store.todos?.map((item) => {
-          return (
-            <li
-              key={item.id}  // React key for list items.
-              className="list-group-item d-flex justify-content-between"
-              style={{ background: item.background }}>
+      <div className="row">
+        {store && store.todos?.map((item) => ( //verifica si store existe, si existe ejecuta .map por cada item retornar el bloque jsx que rendirza una tarjeta con sus datos. 
+            // Tarjeta por contacto
+          <div className="col-md-4 mb4" key={item.id}>
+            <div className="card shadow-sm h-100">
+              <div className="card-body" style={{ background: item.background || "#f8f9fa" }}
+              >                           
+                                          
+              <h5 className="card-title">{item.title}</h5> {/* //titulo del item */}
+              <p className="card-text">           
+                <Link to= {`/single/${item.id}`}>Vied details</Link> {/* Link hacia vista especifica del contacto */}
+              </p>  
+                            <p className="card-text small text-muted">   
+                Open file <code>./store.js</code> to see the global store. {/* mensaje informativo */}  
+              </p>
+              </div>
+            </div>
+          </div>
 
-              {/* Link to the detail page of this todo. */}
-              <Link to={"/single/" + item.id}>Link to: {item.title} </Link>
 
-              <p>Open file ./store.js to see the global store that contains and updates the list of colors</p>
+        ))}
 
-              <button className="btn btn-success"
-                onClick={() => dispatch({
-                  type: "add_task",
-                  payload: { id: item.id, color: '#ffa500' }
-                })}>
-                Change Color
-              </button>
-            </li>
-          );
-        })}
-      </ul>
-      <br />
+      </div>
+
 
     </div>
   );
